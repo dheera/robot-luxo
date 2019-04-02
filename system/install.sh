ed6fd175621ddf6441b9c42e5d3d75e535bd7ada
dheera <dheera@dheera.net>
2019-04-02 06:08:08 +0000

    update

--- system/install.sh
+++ system/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 cat apt-installs.txt | xargs sudo apt-get install -y
-cat pip-installs.txt | xargs sudo pip install --update
-cat pip-installs.txt | xargs sudo pip3 install --update
+cat pip-installs.txt | xargs sudo pip install --upgrade
+cat pip-installs.txt | xargs sudo pip3 install --upgrade
 
 sudo cp -rv root/* /
 sudo systemctl enable autossh
