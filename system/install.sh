94414eb88387aa8ccd3869c75fc83cdc8bd7ba4d
dheera <dheera@dheera.net>
2019-04-02 05:34:42 +0000

    update

---
+++ system/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+cat apt-installs.txt | xargs sudo apt-get install -y
+cat pip-installs.txt | xargs sudo pip install -y
+cat pip-installs.txt | xargs sudo pip3 install -y
+
+sudo cp -rv root/* /
+sudo systemctl enable autossh
+sudo systemctl enable robot
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    ssh-keygen -f id_rsa -t rsa -N ''
+fi
