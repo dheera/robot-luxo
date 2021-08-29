cc54ac4132400943b422ab8e07f35d2e275e69b7
dheera <dheera@dheera.net>
2021-08-29 15:00:48 +0000

    install

--- system/install-ros-base.sh
+++ system/install-ros-base.sh
@@ -14,12 +14,7 @@ if [[ $ISSUE == *"18.04"* ]]; then
 
 	sudo apt update
 
-	sudo apt install -y \
-		ros-melodic-desktop \
-		ros-melodic-ros-control \
-		ros-melodic-control-toolbox \
-		ros-melodic-realtime-tools \
-		ros-melodic-usb-cam
+	sudo apt install -y ros-melodic-desktop
 
 	source /opt/ros/melodic/setup.bash
 
@@ -50,22 +45,6 @@ elif [[ $ISSUE == *"20.04"* ]]; then
 	rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
 
 	sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3
-
-	mkdir -p ~/more_catkin_ws/src
-	cd ~/more_catkin_ws/src
-
-	git clone --branch noetic-devel https://github.com/ros-controls/ros_control
-	git clone --branch melodic-devel https://github.com/ros-controls/control_toolbox
-	git clone --branch noetic-devel https://github.com/ros-controls/realtime_tools
-	git clone --branch noetic-devel https://github.com/ros-perception/image_common
-	git clone --branch develop https://github.com/ros-drivers/usb_cam
-	git clone https://github.com/dheera/rosboard
-	git clone https://github.com/dheera/rosshow
-
-	cd ..
-
-	sudo bash -c "source /opt/ros/noetic/setup.bash && catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3"
-
 fi
 
 
