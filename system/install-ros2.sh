c460279abc9eb462c346e0270b424515e0d78c87
dheera <dheera@dheera.net>
2021-08-30 03:07:56 +0000

    install-ros2.sh

---
+++ system/install-ros2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing ROS2 (Foxy) ..."
+
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+sudo apt install -y \
+	ros-foxy-ros-base \
+	ros-foxy-hardware-interface \
+	ros-foxy-controller-interface \
+	ros-foxy-controller-manager \
+	ros-foxy-joint-state-broadcaster \
+	ros-foxy-joint-state-controller \
+	ros-foxy-joint-state-publisher \
+	ros-foxy-joint-trajectory-controller
+
